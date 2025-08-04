d5be8dc870ba5bbb154a37e40dd8ce48c464c925
Rakesh Kumar <Rakeshkumarr60034@gmail.com>
2025-08-04 18:20:03 +0500

    PRoject is Completed

---
+++ Introduction/currencyConverter/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'], // 👈 This is important
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
