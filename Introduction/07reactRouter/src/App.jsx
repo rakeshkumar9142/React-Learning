6c8cb24ff4c0682d1a2c7ecac6e8bcf0c3c9428c
Rakesh Kumar <Rakeshkumarr60034@gmail.com>
2025-08-16 15:38:30 +0500

    Project is completed

--- Introduction/07reactRouter/src/App.jsx
+++ Introduction/07reactRouter/src/App.jsx
@@ -1,20 +1,18 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import Header from './components/header/header.jsx'
-import Home from './components/home/home.jsx'
-import Footer from './components/footer/footer.jsx'
-
 import './App.css'
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
+import Home from './components/Home/Home'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <Header />
-      <Home />
-      <Footer />
+     
+      
     </>
   )
 }
